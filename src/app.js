d60dad73067252a368301fbee70b7d5e0c9015ed
Brandon <brandonsamuel72@gmail.com>
2020-04-06 03:06:03 -0700

    restructured some more code files

--- src/app.js
+++ src/app.js
@@ -1,24 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+
+// Import Routes
+const userRoutes = require('./routes/users');
+const postRoutes = require('./routes/posts');
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/blog', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(res => console.log('Connected to DB'))
-    .catch(err => console.log(err));
-
 const port = process.env.PORT || 3000;
-
-const homeRoutes = require('./routes/home');
-const postRoutes = require('./routes/posts');
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
 
 app.use('/blog-post', postRoutes);
-app.use(homeRoutes);
+app.use(userRoutes);
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
